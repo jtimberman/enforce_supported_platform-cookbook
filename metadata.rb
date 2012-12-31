f2c5236bb176b55f6cb65de7ef0772a5f710679d
Joshua Timberman <joshua@opscode.com>
2012-12-30 21:21:11 -0700

    More accurate description of this cookbook

--- metadata.rb
+++ metadata.rb
@@ -2,6 +2,6 @@ name             "enforce_supported_platform"
 maintainer       "Joshua Timberman"
 maintainer_email "opensource@housepub.org"
 license          "Apache 2.0"
-description      "Installs/Configures enforce_supported_platform"
+description      "Provides Chef::Recipe helper methods for enforcing specific platforms in cookbook metadata"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
